cc182781cd77df7263f1e6a6e69fb7bac363f858
Eliezbaba <ezeugahe@gmail.com>
2023-03-17 13:39:56 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print all possible combinations of single-digit numbers'
+ * Return: always 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 48; k <= 57; k++)
+	{
+		putchar(k);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
